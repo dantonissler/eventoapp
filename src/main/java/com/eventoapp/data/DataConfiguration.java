461883a2a597929bec01b8d072bfede63164d7bc
Danton Issler Rodrigues <danton.issler18@gmail.com>
2018-08-10 16:20:28 -0400

    Colocando configuração Spring para diferenciar os ambientes de deselvolvimento e produção

--- src/main/java/com/eventoapp/data/DataConfiguration.java
+++ src/main/java/com/eventoapp/data/DataConfiguration.java
@@ -1,29 +1,38 @@
+
 package com.eventoapp.data;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import javax.sql.DataSource;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
 
 	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("postgres://<cklfnxazyikewb>:<188368d1b13c43d59e6652d6c4f40a2ee6f2b23f7c88a2092703dc76344e3a2b>@<ec2-54-235-212-58.compute-1.amazonaws.com>/<ddf0p5602o7ora>"));
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+		dataSource.setUsername("root");
+		dataSource.setPassword("toor");
+		return dataSource;
+	}
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
-
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-
-        return basicDataSource;
-    }
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setPrepareConnection(true);
+		return adapter;
+	}
 }
